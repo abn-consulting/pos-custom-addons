f9f89ddb47ea3ddf91e89798ff8eea76349b63c2
BAWA <info@abn.consulting>
2026-05-10 12:39:08 +0300

    Harden POS access rights render guards

--- sensible_pos_access_rights_employee/static/src/overrides/components/generic_components/list_container/sbl_list_container.js
+++ sensible_pos_access_rights_employee/static/src/overrides/components/generic_components/list_container/sbl_list_container.js
@@ -13,19 +13,19 @@ patch(ListContainer.prototype, {
 
     isCreateNewOrderButtonVisible() {
         const employee = this.pos.cashier;
-        return employee && !employee.sbl_hide_pos_new_order_button;
+        return !employee || !employee.sbl_hide_pos_new_order_button;
     },
 });
 
 patch(ListContainer, {
     template: xml`
-        <div class="overflow-hidden d-flex flex-grow-1" t-attf-class="{{props.class}}">
-            <button t-if="props.onClickPlus and isCreateNewOrderButtonVisible()" class="list-plus-btn btn btn-secondary btn-lg me-1" t-on-click="props.onClickPlus">
+        <div class="d-flex flex-grow-1" t-attf-class="{{props.class}}" t-att-class="{'overflow-hidden': !isUiSmall}">
+            <button t-if="props.onClickPlus and isCreateNewOrderButtonVisible()" class="list-plus-btn btn btn-secondary btn-lg me-1 my-2" t-on-click="props.onClickPlus">
                 <i class="fa fa-fw fa-plus-circle" aria-hidden="true"/>
             </button>
             <button t-if="this.sizing.isLarger or props.forceSmall" t-on-click="toggle"
-                class="btn btn-secondary mx-1 fa fa-caret-down" />
-            <div class="overflow-hidden w-100 position-relative">
+                class="btn btn-secondary mx-1 fa fa-caret-down my-2" />
+            <div class="overflow-hidden w-100 position-relative py-2">
                 <div t-ref="container" class="list-container-items d-flex w-100">
                     <div t-if="!props.forceSmall" t-foreach="props.items" t-as="item" t-key="item_index" t-att-class="{'invisible': shouldBeInvisible(item_index)}">
                         <t t-slot="default" item="item"/>
